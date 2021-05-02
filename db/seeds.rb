2bb5d040b30bb0307ffefb5112f41f297497caf9
Evan <evanahouse@gmail.com>
2021-05-02 01:19:00 -0700

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SchoolClass.destroy_all
+Student.destroy_all
+
+SchoolClass.create(title: 'Math', room_number: 101)
+SchoolClass.create(title: 'Reading', room_number: 201)
+SchoolClass.create(title: 'Writing', room_number: 301)
+SchoolClass.create(title: 'Literature', room_number: 401)
+
+Student.create(first_name: 'Evan', last_name: 'House')
+Student.create(first_name: 'Forrest', last_name: 'Gates')
+Student.create(first_name: 'Tamsen', last_name: 'Fuhs')
